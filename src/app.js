a3fd278bc2985d8e446e72d11899e02be9316b80
Abhishekk3003 <abhishekrawat3062@gmail.com>
2025-06-12 19:01:27 +0530

    exploring get and post request

--- src/app.js
+++ src/app.js
@@ -4,15 +4,31 @@ const app = express();
 
 const PORT =  3000;
 
+
+// app.use("/",(req,res)=> {
+//   res.send('Hello world')
+// }) 
+// any route which is start from this '/' this route handler is handle it
+
+
+
+app.get('/user',(req,res)=>{
+    res.send({firstName: 'John', lastName: 'Doe'});
+});
+
+app.post('/user',(req,res)=>{
+    res.send('Hello user from server with post method')
+});
+
+
 app.use("/test", (req,res)=>{
     res.send('hello client/user from server')
 })
 
-
-app.use((req,res)=>{
-    res.send('Hello, World!');
-})
-
+// app.use((req,res)=>{
+//     res.send('Hello, World!');
+// })
+// // if you want to handle all the request which is not handled by any route handler then you can use this
 
     
 
